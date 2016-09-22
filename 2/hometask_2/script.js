d182ec2a9989b97f1434c856409d5ad72336b782
fenixsarov <fenixsarov@gmail.com>
2016-09-22 21:09:25 +0300

    Выложено ДЗ к третьему уроку

--- 2/hometask_2/script.js
+++ 2/hometask_2/script.js
@@ -1,1 +1,33 @@
 // Home task for lesson 2
+
+//Решение Алены
+var a = 0;
+while (a < 100) {
+    a++;
+    var str = "";
+    if ((a % 5) == 0) {
+        str = "Buzz";
+    }
+    if ((a % 3) == 0) {
+        str = str + "Fizz";
+    }
+    if (str == "") {
+        str = a;
+    }
+    console.log(str);
+}
+var a = 0;
+while (a < 100) {
+    a++;
+    var str = "";
+    if ((a % 5) == 0) {
+        str = "Buzz";
+    }
+    if ((a % 3) == 0) {
+        str = str + "Fizz";
+    }
+    if (str == "") {
+        str = a;
+    }
+    console.log(str);
+}
---
+++ 3/hometask_3/script.js
@@ -0,0 +1,33 @@
+/**
+ * Created by home on 22.09.2016.
+ */
+
+// Рисуем октоторпами треугольник
+var octo = "#";
+for( var i = 0; i < 7; ++i ) {
+    console.log( octo );
+    octo += "#";
+}
+
+// Разделитель
+console.log("\n");
+
+// Рисуем "шахматную доску"
+var hash = "#";
+var hash0 = " ";
+for ( var j = 1; j <= 8; ++j ) {
+    if ( j%2 == 0) {
+        var line_even = "";
+        for ( var k = 0; k < 4; ++k) {
+            line_even += hash + hash0;
+        }
+        console.log( line_even );
+    }
+    else {
+        var line_uneven = "";
+        for ( var k = 0; k < 4; ++k) {
+            line_uneven += hash0 + hash;
+        }
+        console.log( line_uneven );
+    }
+}
