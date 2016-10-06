e5d5d716c332144cf53c192b64b56a9e46e8d8e3
fenixsarov <fenixsarov@gmail.com>
2016-10-06 20:07:47 +0300

    Незначительная правка

--- 6/hometask_6/script2.js
+++ 6/hometask_6/script2.js
@@ -1,3 +1,25 @@
 /**
  * Created by fenixsarov on 06.10.16.
  */
+// Функция получает массив как аргумент и выдаёт новый массив, с обратным порядком элементов. 
+function reverseArray(array) {
+    var result = [];
+    for (var i = array.length - 1; i >= 0; i--) {
+        result.push(array[i]);
+    }
+    return result;
+}
+
+console.log(reverseArray([1, 2, 3]));
+
+function reverseArrayInPlace(array) {
+    for (var i = 0; i < Math.floor(array.length / 2); i++) {
+        var old_array = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = old_array;
+    }
+    return array;
+}
+var arr = [1, 2, 3, 4];
+reverseArrayInPlace(arr);
+console.log(arr);
